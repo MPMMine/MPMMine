0498c779e5027fc3b8f95d7100a6e2773bfb841d
tpawlak <tpawlak@cs.put.poznan.pl>
2026-09-04 16:06:23 +0200

    feat: add version information

--- scripts/make_packages.sh
+++ scripts/make_packages.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-declare VERSION="0.1.0.20260722"
+set -e
+
+# Update the VERSION file to change version information
+VERSION="$(cat VERSION)"
+declare VERSION
 
 cd "$(dirname -- "${BASH_SOURCE[0]}")/.." || exit
 
 # Build 7z package
-git ls-tree -r --name-only HEAD | grep -e .dzn -e .mzn -e .md -e .json -e .bib -e .png -e .svg -e LICENSE > filelist
-rm -f MPMMine-v$VERSION.7z
-LC_ALL=C 7zz a -m0=lzma2 -mmt16 -mx9 -mfb=273 -md=256M -ms=8G -slp -sse -ssp -y MPMMine-v$VERSION.7z @filelist
+git ls-tree -r --name-only HEAD | grep -e .dzn -e .mzn -e .md -e .json -e .bib -e .png -e .svg -e LICENSE -e VERSION > filelist
+rm -f "MPMMine-v$VERSION.7z"
+LC_ALL=C 7zz a -m0=lzma2 -mmt16 -mx9 -mfb=273 -md=256M -ms=8G -slp -sse -ssp -y "MPMMine-v$VERSION.7z" @filelist
 rm filelist
 
 # Build SQLite-zstd package
-python3 -m mpmmine.sqlite_backend -t -c zstd . MPMMine-zstd-v$VERSION.sqlite
+python3 -m mpmmine.sqlite_backend -t -c zstd . "MPMMine-zstd-v$VERSION.sqlite"
 
 # Build SQLite package and compress
-python3 -m mpmmine.sqlite_backend -t . MPMMine-v$VERSION.sqlite
-xz -9ek MPMMine-v$VERSION.sqlite
+python3 -m mpmmine.sqlite_backend -t . "MPMMine-v$VERSION.sqlite"
+xz -k --lzma2=preset=9e,dict=512MiB,nice=273,depth=1000 "MPMMine-v$VERSION.sqlite"
+
